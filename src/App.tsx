80400b09818f6ce6a7e33287c035917c507de85f
faruqalawy <faruqhudaya@gmail.com>
2023-10-27 14:55:42 +0700

    second commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+
+const defaultBreakTime = 5 * 60;
+const defaultSessionTime = 25 * 60;
+const min = 60;
+const max = 60 * 60;
+const interval = 60;
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>25 + 5 Clock - by Faruq</h1>
+      
+      <div id="break-label">Lama Istirahat</div>
+      <button id="break-increment">+</button>
+      <button id="break-decrement">-</button>
+
+      <div id="session-label">Lama Sesi</div>
+      <button id="session-increment">+</button>
+      <button id="session-decrement">-</button>
     </>
   )
 }
